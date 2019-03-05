5c6db375aaa566960235c91fa6066608d544134d
dguco <1139140929@qq.com>
2019-03-05 19:17:39 +0800

    task interface

--- examples/main.cpp
+++ examples/main.cpp
@@ -130,32 +130,36 @@ int main(int argc, char *argv[])
     C *c = new C;
     c->test();
     int test_a = 10;
-    auto task1 = stagefuture::run_async([test_a]() -> void
+    stagefuture::threadpool_scheduler scheduler(1);
+    auto task1 = stagefuture::run_async(scheduler,
+                                        [test_a]() -> void
                                         {
                                             std::cout << "Task 1 executes asynchronously,test_a * test_a: "
                                                       << test_a * test_a << std::endl;
                                         });
-    auto task2 = stagefuture::supply_async([]() -> int
+    auto task2 = stagefuture::supply_async(scheduler,
+                                           []() -> int
                                            {
                                                std::cout << "Task 2 executes in parallel with stage_future 1"
                                                          << std::endl;
                                                return 42;
                                            });
-    auto task3 = task2.thenApply([](int value) -> int
-                            {
-                                std::cout << "Task 3 executes after stage_future 2, which returned "
-                                          << value << std::endl;
-                                return value * 3;
-                            });
+    auto task3 = task2.thenApplyAsync(scheduler,
+                                      [](int value) -> int
+                                      {
+                                          std::cout << "Task 3 executes after stage_future 2, which returned "
+                                                    << value << std::endl;
+                                          return value * 3;
+                                      });
     auto task4 = stagefuture::when_all(task1, task3);
     auto
-        task5 = task4.then([](std::tuple<stagefuture::stage_future<void>,
-                                         stagefuture::stage_future<int>>
-                              results)
-                           {
-                               std::cout << "Task 5 executes after tasks 1 and 3. Task 3 returned "
-                                         << std::get<1>(results).get() << std::endl;
-                           });
+        task5 = task4.thenApplyAsync([](std::tuple<stagefuture::stage_future<void>,
+                                                   stagefuture::stage_future<int>>
+                                        results)
+                                     {
+                                         std::cout << "Task 5 executes after tasks 1 and 3. Task 3 returned "
+                                                   << std::get<1>(results).get() << std::endl;
+                                     });
 
     task5.get();
     std::cout << "Task 5 has completed" << std::endl;
